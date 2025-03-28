8658cbc663382b520f52342cbd3529e31c62fd1f
YanWaltz <yanwaltaz@gmail.com>
2025-03-28 01:41:29 -0300

    Correção do print

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Boa noitee!");
+   
+   
+   
+   
     }
 }
