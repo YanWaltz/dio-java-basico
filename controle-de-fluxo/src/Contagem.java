27963f9de7b38ba1909f9e73c4878644579c7012
YanWaltz <yanwaltaz@gmail.com>
2025-04-01 23:46:31 -0300

    uso do for

---
+++ controle-de-fluxo/src/Contagem.java
@@ -0,0 +1,9 @@
+public class Contagem {
+    public static void main(String[] args) {
+        for (int carneirinhos = 2; carneirinhos<=100; carneirinhos++) {
+            System.out.println("Tem " +carneirinhos + " carneirinhos");
+        }
+            System.out.println("\nOu seja, tem carneirinho pra caraca\n");
+    }
+    
+}
